77ddf258aa822d5dcb97c243811f961502dbfc2e
Suw16 <36479269+Suw16@users.noreply.github.com>
2018-02-16 08:41:49 +0700

    git commit

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
-echo "Hello World, By Pai";
-?>
+include_once('connent_db.php');
+
+if(! empty($_GET['id'])&& !empty($_GET['name'])){
+
+$name = $_GET['id'];
+$id = $_GET['name'];
+
+
+$sql = "INSERT INTO student(idstudent,name)
+VALUES('$name','$id')";
+$conn ->exec ($sql);
+header("Location: index.php");
+}
+
+?> 
+<form metod="GET" action="index.php">
+<input type=text name="id" placeholder="ID"><br><br>
+<input type=text name="name" placeholder="Name"><br><br>
+
+
+<input type="submit">
+
+</form>
