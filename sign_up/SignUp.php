53a35b844bb9a0233f0e77971844a3c06109c289
Suw16 <suwat16128_kub@hotmail.co.th>
2018-02-25 15:52:19 +0700

    Hash

--- sign_up/SignUp.php
+++ sign_up/SignUp.php
@@ -4,9 +4,11 @@ include('connect_db.php');
 
 $Fname= $_POST['name'];
 $Email = $_POST['email'];
-$Pass = $_POST['pass'];
+$Pass = password_hash($_POST['pass'],PASSWORD_DEFAULT);
+
 
 $sql =  "INSERT INTO teacher(fullname,email,password)
 VALUES('$Fname','$Email','$Pass')";
 $conn ->exec($sql);
+
 ?>
