064265809982d16b611c6712daa3c57c3fc1c8dc
Codethm <dong55.102375@gmail.com>
2018-02-25 21:20:17 +0700

    confuse

--- index/login.php
+++ index/login.php
@@ -4,32 +4,27 @@ include_once('connect_db.php');
 $email = $_POST['email'];
 $pass = $_POST['pass'];
 
-$sql = "SELECT password FROM teacher WHERE email = '$email'&& password='$pass';";
+$sql = "SELECT idteacher,password FROM teacher WHERE email = '$email';";
 echo $sql;
 
 $stmt = $conn->prepare($sql);
 if($stmt->execute()){
-    // Check if username exists, if yes then verify password
     if($stmt->rowCount() == 1){
         if($row = $stmt->fetch()){
-            $hashed_password = strval($row[0]);
-            echo("||".$hashed_password."||".$pass."||");
-            if($hashed_password == $pass){
-                /* Password is correct, so start a new session and
-                save the username to the session */
+            $hashed_password = $row['password'];
+            if(password_verify($pass,$hashed_password)){
                 session_start();
-                $_SESSION['email'] = $email;      
+                $_SESSION['pk'] = $row['idteacher'];     
                 header("location: http://127.0.0.1/Etal/students/");
             } else{
-                // Display an error message if password is not valid
-                $password_err = 'The password you entered was not valid.';
+                $message = 'The password you entered was not valid.';
+                echo "<script type='text/javascript'>alert('$message');</script>";
             }
         }
     } else{
-        // Display an error message if username doesn't exist
-        $username_err = 'No account found with that username.';
+        $message = 'No account found with that username.';
+        echo "<script type='text/javascript'>alert('$message');</script>";
     }
 } else{
     echo "Oops! Something went wrong. Please try again later.";
 }
-echo $username_err.$password_err;
