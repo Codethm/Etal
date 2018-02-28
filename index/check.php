3fb5a8517c1bfaf382816639836bb46b35941cf4
Suw16 <suwat16128_kub@hotmail.co.th>
2018-02-28 21:12:39 +0700

    Dong commit

--- index/check.php
+++ index/check.php
@@ -3,6 +3,20 @@ include_once('connect_db.php');
 
 if(!empty($_GET['IDStudent']))
 {
+	date_default_timezone_set('Asia/Bangkok');
+	$date = date('Y-m-d H:i:s');
+	$starttime = date("Y-m-d H:i:s",strtotime($date."-30 minute"));
+	$endtime = date("Y-m-d H:i:s",strtotime($date."+30 minute"));
+	
+	$sql = "SELECT idclass FROM `class` WHERE `date` BETWEEN '$starttime' AND '$endtime'";
+	echo $sql;
+	$stmt =$conn->prepare($sql);
+	$stmt->execute();
+
+	$result = $stmt->fetch();
+	$idclass = $result['idclass'];
+	echo "||||||||||||".$idclass;
+	
 
 	$idstu = $_GET['IDStudent'];
 	$sql = "SELECT idstudent FROM student WHERE code = '$idstu';";
@@ -10,8 +24,9 @@ if(!empty($_GET['IDStudent']))
     $stmt->execute();
 	$row = $stmt->fetch();
 	$idrow = $row[0];
-	$date = date('Y-m-d H:i:s');
-    $sql = "INSERT INTO attendance(student_idstudent,class_idclass,time) VALUE('$idrow','2','$date');";
+
+	$sql = "INSERT INTO attendance(student_idstudent,class_idclass,time) VALUE('$idrow',$idclass,'$date');";
+	echo $sql;
     $conn->exec($sql);
 	header('location: check.html');
 }
