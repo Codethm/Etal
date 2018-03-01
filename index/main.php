33b80afbc9cdf6a59d9cee634080c344b9266cc2
Suw16 <suwat16128_kub@hotmail.co.th>
2018-03-02 01:20:03 +0700

    main delete copy

--- index/main.php
+++ index/main.php
@@ -30,37 +30,6 @@ $idth = $_SESSION['idteacher'];
 </head>
 
 <body>
-
-
-	<html>
-
-	<head>
-		<title>Insert Student</title>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!--===============================================================================================-->
-		<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
-		<!--===============================================================================================-->
-		<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-		<!--===============================================================================================-->
-		<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-		<!--===============================================================================================-->
-		<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-		<!--===============================================================================================-->
-		<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-		<!--===============================================================================================-->
-		<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-		<!--===============================================================================================-->
-		<link rel="stylesheet" type="text/css" href="css/util.css">
-		<link rel="stylesheet" type="text/css" href="css/main.css">
-		<!--===============================================================================================-->
-		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-
-	</head>
-
-	<body>
 		<div class="w3-top">
 			<div class="w3-bar w3-white w3-card" id="myNavbar">
 				<a href="main.php" class="w3-bar-item w3-button w3-wide ">ETAL</a>
@@ -139,25 +108,6 @@ $idth = $_SESSION['idteacher'];
 		</div>
 
 
-
-		<!--===============================================================================================-->
-		<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-		<!--===============================================================================================-->
-		<script src="vendor/bootstrap/js/popper.js"></script>
-		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-		<!--===============================================================================================-->
-		<script src="vendor/select2/select2.min.js"></script>
-		<!--===============================================================================================-->
-		<script src="vendor/tilt/tilt.jquery.min.js"></script>
-		<script>
-			$('.js-tilt').tilt({
-				scale: 1.1
-			})
-		</script>
-		<!--===============================================================================================-->
-		<script src="js/main.js"></script>
-
-	</body>
 
 	</html>
 
